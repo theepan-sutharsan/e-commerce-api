eca90abe79f1c733f7401067c335fac5c0b4eb9e
Kamsika <kamsika0401@gmail.com>
2026-06-27 11:19:49 +0530

    rechange in config.py file

--- app/config.py
+++ app/config.py
@@ -1,20 +1,24 @@
-import os 
-from  dotenv import load_dotenv
+from datetime import timedelta
+import os
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
-class config:
-    DB_USER = os.getenv("DB_USER","root")
-    DB_PASSWORD =os.getenv("DB_PASSWORD","root123")
-    DB_HOST =os.getenv("localhost")
-    DB_NAME=os.getenv("DB_NAME","student_data")
 
-    SQLALCHEMY_DATABASE_URI=(
+class Config:
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "root123")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_NAME = os.getenv("DB_NAME", "student_data")
+
+    SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     )
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key-change-me")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(
         minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES_MINUTES", "15"))
     )
+
+
