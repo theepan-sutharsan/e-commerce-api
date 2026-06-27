d06e2a9ab87a16511ad38651235599d0e5120e14
Kamsika <kamsika0401@gmail.com>
2026-06-27 10:37:46 +0530

    cofig.py final commit

--- app/config.py
+++ app/config.py
@@ -10,5 +10,7 @@ class config:
     DB_NAME=os.getenv("DB_NAME","student_data")
 
     SQLALCHEMY_DATABASE_URI=(
-        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST} "
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     )
+
+    SQLALCHEMY_TRACK_MODIFICATION = False
