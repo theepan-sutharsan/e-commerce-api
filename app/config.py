2f69c00e8e7315d661b6f398aa3bde0f2b90c91e
Kamsika <kamsika0401@gmail.com>
2026-06-27 10:34:34 +0530

    config.py created

---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os 
+from  dotenv import load_dotenv
+
+load_dotenv()
+
+class config:
+    DB_USER = os.getenv("DB_USER","root")
+    DB_PASSWORD =os.getenv("DB_PASSWORD","root123")
+    DB_HOST =os.getenv("localhost")
+    DB_NAME=os.getenv("DB_NAME","student_data")
+
+    SQLALCHEMY_DATABASE_URI=(
+        f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST} "
+    )
